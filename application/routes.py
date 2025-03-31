5864d2603111163265ef130ff192022af9f5b349
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-31 10:55:12 +0200

    Added dynamic json data, active navbar higlighting

--- application/routes.py
+++ application/routes.py
@@ -1,20 +1,23 @@
 from application import app
 from flask import render_template
+import json
 
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html", login=False, index=True)
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html")
+    with open("courses.json", "r") as file:
+        courseList = json.load(file)
+    return render_template("courses.html", courses=courseList, course=True)
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("register.html", register=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", login=True)
