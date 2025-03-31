510499c3d3cdf350255321cef4b9c93595f4c476
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-31 11:59:04 +0200

    Added enrollment form and route

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import render_template
+from flask import render_template, request
 import json
 
 @app.route("/")
@@ -8,11 +8,12 @@ import json
 def index():
     return render_template("index.html", login=False, index=True)
 
-@app.route("/courses")
-def courses():
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term="2025"):
     with open("courses.json", "r") as file:
         courseList = json.load(file)
-    return render_template("courses.html", courses=courseList, course=True)
+    return render_template("courses.html", courses=courseList, course=True, term=term)
 
 @app.route("/register")
 def register():
@@ -21,3 +22,14 @@ def register():
 @app.route("/login")
 def login():
     return render_template("login.html", login=True)
+
+@app.route("/enrollment", methods=["GET"])
+def enrollment():
+    id = request.args.get("courseID")
+    title = request.args.get("title")
+    term = request.args.get("term")
+    return render_template("enrollment.html", data={
+        "id": id,
+        "title": title,
+        "term": term,
+    })
